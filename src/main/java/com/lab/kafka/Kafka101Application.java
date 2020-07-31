2a7a2f0c60a05d6f239fa1dc92e7644bc6df4c9d
Christophe Douy <christophe.douy49@gmail.com>
2020-07-31 12:19:55 +0200

    init project

---
+++ src/main/java/com/lab/kafka/Kafka101Application.java
@@ -0,0 +1,13 @@
+package com.lab.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kafka101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kafka101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab/kafka/Kafka101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kafka101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
